0e7a7b146553edacf6a476a54aa0fd8d851a0612
Victor <victormoraes.2002@hotmail.com>
2024-06-21 00:25:35 -0300

    Adding Catalog.API Infrasctructure , Handler and Endpoint Classes

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found!")
+        {
+
+        }
+    }
+
+}
