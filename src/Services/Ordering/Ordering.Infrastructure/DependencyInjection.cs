cf54547d4828a116a3f5c65c3a18dcb3e897d96b
Victor Moraes <victormoraes.2002@hotmail.com>
2025-03-09 14:51:13 -0300

    ✨ Novos pacotes: Adicionados Microsoft.FeatureManagement.AspNetCore e Microsoft.EntityFrameworkCore.Design.
    📦 Atualizações no projeto: Incluídas referências e dependências nos arquivos do projeto.
    
    🏗️ Modelos de domínio: Implementados Customer, Order, OrderItem e Product com abstrações e objetos de valor.
    
    ⚡ Eventos: Adicionados OrderCreatedEvent e OrderUpdatedEvent para tratamento de eventos.
    
    🛠️ Banco de dados: Criado ApplicationDbContext e adicionada lógica de seeding.
    
    🕵️‍♂️ Auditoria: Configurado AuditableEntityInterceptors para rastreamento de mudanças.
    
    🐳 Docker: Atualizadas configurações para incluir o serviço SQL Server.

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure
 {
@@ -10,6 +13,12 @@ namespace Ordering.Infrastructure
             IConfiguration configuration)
         {
             string connectionString = configuration.GetConnectionString("Database")!;
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.AddInterceptors(new AuditableEntityInterceptors());
+                options.UseSqlServer(connectionString);
+            });
 
             return services;
         }
